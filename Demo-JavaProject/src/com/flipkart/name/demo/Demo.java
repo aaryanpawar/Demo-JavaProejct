015b011df07bc58fad45857766509ce6f52c6c08
Aaryan Pawar <aaryan.pawar@BLRETV-C02GL053.local>
2023-05-17 17:32:18 +0530

    First commit

---
+++ Demo-JavaProject/src/com/flipkart/name/demo/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.name.demo;
+
+/**
+ * @author aaryan.pawar
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
